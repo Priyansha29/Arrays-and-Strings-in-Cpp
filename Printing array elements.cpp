e2716b274a8b0236555f818ec8935d806e3b82a4
Priyansha29 <priyanshagour29@gmail.com>
2025-07-29 14:50:28 +0530

    Create Printing array elements.cpp

---
+++ Printing array elements.cpp
@@ -0,0 +1,22 @@
+//Priyansha Gour
+//24070123079
+//ENTC-A3
+
+#include <iostream>
+using namespace std;
+int main() {
+    int marks[5]={78,66,79,90,56};
+    for(int i=0;i<5;i++){
+        cout<<marks[i]<<endl;
+    }
+
+    return 0;
+}
+
+/*output:
+78
+66
+79
+90
+56
+*/
