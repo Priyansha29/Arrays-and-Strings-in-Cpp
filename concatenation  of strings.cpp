cc224a7170a8894b84707ab8f6511a7307e09d00
Priyansha29 <priyanshagour29@gmail.com>
2025-07-31 11:34:47 +0530

    Create concatenation  of strings.cpp

---
+++ concatenation  of strings.cpp
@@ -0,0 +1,15 @@
+//Priyansha Gour
+//24070123079
+//ENTC-A3
+#include <iostream>
+using namespace std;
+int main() {
+    string a, b;
+    cout<<"Enter string a : ";
+    cin>>a;
+    cout<<"Enter string b : ";
+    cin>>b;
+    cout<<a<<" "<<b;
+    
+    return 0;
+}
