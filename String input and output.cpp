92c90315db8126f2a20c59d88f5a1a51e21b2339
Priyansha29 <priyanshagour29@gmail.com>
2025-07-31 11:03:56 +0530

    Create String input and output.cpp

---
+++ String input and output.cpp
@@ -0,0 +1,15 @@
+//Priyansha Gour
+//24070123079
+//ENTC-A3
+
+
+include <iostream>
+using namespace std;
+int main() {
+    string a;
+    cout<<"Enter string a : ";
+    cin>>a;
+    cout<<"Given string is : "<<a;
+    return 0;
+}
+
